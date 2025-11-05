d2fe50788caff5cf9e671ba562e81a87c2cd3d31
Jannis Naeve <jannis.naeve@haw-hamburg.de>
2025-11-05 13:40:06 +0100

    Make wallpaper picker non repetitive

--- hyprpaper_picker.sh
+++ hyprpaper_picker.sh
@@ -3,10 +3,26 @@ export HYPRLAND_INSTANCE_SIGNATURE=$(ls /run/user/1000/hypr/ | head -n 1)
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 DIR="${HOME}/Pictures/wallpapers"
+CACHE_FILE="${HOME}/.cache/last_wallpaper"
 
 if [[ -d "${DIR}" ]]; then
     files=($(ls "${DIR}"))
-    random_file=${files[RANDOM % ${#files[@]}]}
+else
+    exit 1
 fi
+
+if [[ -f "${CACHE_FILE}" ]]; then
+    last_wallpaper=$(<"${CACHE_FILE}")
+else 
+    last_wallpaper=""
+fi
+
+while true; do
+    random_file=${files[RANDOM % ${#files[@]}]}
+    [[ "$random_file" != "$last_wallpaper" ]] && break
+done
+
+echo "${random_file}" > "${CACHE_FILE}"
+
 hyprctl hyprpaper reload ,"${DIR}/${random_file}" 
 notify-send "Wallpaper changed"
